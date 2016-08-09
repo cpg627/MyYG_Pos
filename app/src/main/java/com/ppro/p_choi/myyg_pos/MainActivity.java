b927031a407685a6b5c51be6404628290fb86378
choi <cpg627@gmail.com>
2016-08-10 01:22:32 +0900

    기본 성공. 선택과 리스트 작동.

--- app/src/main/java/com/ppro/p_choi/myyg_pos/MainActivity.java
+++ app/src/main/java/com/ppro/p_choi/myyg_pos/MainActivity.java
@@ -1,16 +1,21 @@
 package com.ppro.p_choi.myyg_pos;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_1_1, btn_1_2;
     ListView listView1;
+    ArrayAdapter<String> m_adapter;
     String[] selected_items = {"가", "나", "다", "라"};
 
     @Override
@@ -21,26 +26,28 @@ public class MainActivity extends AppCompatActivity {
         //버튼에 리스너 달기
         btn_1_1=(Button)findViewById(R.id.btn_1_1);
         btn_1_2=(Button)findViewById(R.id.btn_1_2);
+       //어댑터 생성
+        m_adapter = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1);
         //리스트뷰 생성
         listView1 = (ListView)findViewById(R.id.listview1);
-        listView1.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);//선택이 가능하도록 설정
-        //어댑터 생성
-        final ArrayAdapter<String> selected_adapter = new ArrayAdapter<String>(
-                getApplicationContext(),
-                android.R.layout.simple_list_item_multiple_choice,
-                selected_items);
-        //리스트에 어댑터 추가
-        listView1.setAdapter(selected_adapter);
+        listView1.setOnItemClickListener(onClickListItem);
+        //리스트에 어댑터 추가** 에러가 발생하는 부분
+        listView1.setAdapter(m_adapter);
+        listView1.setBackgroundColor(Color.GRAY);//배경색 바꾸기
 
         Button.OnClickListener listener = new Button.OnClickListener(){
             public void onClick(View v){
                 switch (v.getId()){
                     case R.id.btn_1_1:
                         System.out.println("1_1 눌림");
-                        selected_adapter.add("Americano");
+                        //ListView에 Item추가
+                        m_adapter.add("Altic Armor");
                     case R.id.btn_1_2:
                         System.out.println("1_2 눌림");
-                        selected_adapter.add("letter");
+                        //ListView에 Item추가
+                        m_adapter.add("F-TR");
                         break;
                 }
             }
@@ -49,6 +56,13 @@ public class MainActivity extends AppCompatActivity {
         btn_1_2.setOnClickListener(listener);
 
     }
-    //이벤트 핸들러
+    //listItem 터치 이벤트
+    private OnItemClickListener onClickListItem = new OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //이벤트 클릭시 해당 아이템 위치의 텍스트를 출력
+            Toast.makeText(getApplicationContext(), m_adapter.getItem(position), Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
